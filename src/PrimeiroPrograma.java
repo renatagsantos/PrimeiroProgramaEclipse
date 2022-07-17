dfb32426ab41c992acc8bf3ce1e63c9493a4b9fe
Renata Santos <contato.renatagsantos@gmail.com>
2022-07-17 11:33:30 -0300

    primeiro commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+import model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		//Livros livros = new Livros();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello, world!");
+		System.out.println("Resultado:" + (a*b));*/
+	}
+	
+public class Livros {
+	
+	private String nome2;
+	private String npag;
+	
+}
+
+}
